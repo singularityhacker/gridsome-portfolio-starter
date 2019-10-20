6a01f1ea9d20ddb3a0bcae3ecdb58c17e2e40a06
Justice Conder <justiceconder@gmail.com>
2019-10-19 22:20:00 -0400

    Added Netlify CMS

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -2,6 +2,7 @@
   <Layout>
     <div class="container-inner mx-auto my-16">
       <h1 class="text-4xl font-bold leading-tight">{{ $page.post.title }}</h1>
+      <!-- <img :src="$page.post.featuredImage" alt="blog"> -->
       <div class="text-xl text-gray-600 mb-4">{{ $page.post.date }}</div>
       <div class="flex mb-8 text-sm">
         <g-link
@@ -45,4 +46,3 @@ export default {
 </script>
 
 <style src="../css/github-markdown.css" />
-
