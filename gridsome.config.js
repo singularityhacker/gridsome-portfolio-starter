f1f85ee6398969c42f186b8e56e6b50f69f855bb
0xJustice.eth <justiceconder@gmail.com>
2024-01-19 17:54:38 -0500

    remove blog

--- gridsome.config.js
+++ gridsome.config.js
@@ -30,41 +30,41 @@ module.exports = {
       ],
       }
     },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: 'blog/**/*.md',
-        typeName: 'Post',
-        refs: {
-          tags: {
-            typeName: 'Tag',
-            create: true
-          }
-        }
-      }
-    },
-    {
-      use: 'gridsome-plugin-rss',
-      options: {
-        contentTypeName: 'Post',
-        feedOptions: {
-          title: 'Gridsome Portfolio Starter Blog',
-          feed_url: 'https://gridsome-portfolio-starter.netlify.com/rss.xml',
-          site_url: 'https://gridsome-portfolio-starter.netlify.com/'
-        },
-        feedItemOptions: node => ({
-          title: node.title,
-          description: node.summary,
-          url: 'https://gridsome-portfolio-starter.netlify.com' + node.path,
-          author: 'Andre Madarang',
-          date: node.date
-        }),
-        output: {
-          dir: './static',
-          name: 'rss.xml'
-        }
-      }
-    },
+    // {
+    //   use: '@gridsome/source-filesystem',
+    //   options: {
+    //     path: 'blog/**/*.md',
+    //     typeName: 'Post',
+    //     refs: {
+    //       tags: {
+    //         typeName: 'Tag',
+    //         create: true
+    //       }
+    //     }
+    //   }
+    // },
+    // {
+    //   use: 'gridsome-plugin-rss',
+    //   options: {
+    //     contentTypeName: 'Post',
+    //     feedOptions: {
+    //       title: 'Gridsome Portfolio Starter Blog',
+    //       feed_url: 'https://gridsome-portfolio-starter.netlify.com/rss.xml',
+    //       site_url: 'https://gridsome-portfolio-starter.netlify.com/'
+    //     },
+    //     feedItemOptions: node => ({
+    //       title: node.title,
+    //       description: node.summary,
+    //       url: 'https://gridsome-portfolio-starter.netlify.com' + node.path,
+    //       author: 'Andre Madarang',
+    //       date: node.date
+    //     }),
+    //     output: {
+    //       dir: './static',
+    //       name: 'rss.xml'
+    //     }
+    //   }
+    // },
     {
       use: '@gridsome/plugin-sitemap',
       options: {
